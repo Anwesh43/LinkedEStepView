c11bcd09add4fa9d82866a688d3ccbce0e6047af
Anwesh43 <anweshthecool0@gmail.com>
2018-10-13 08:54:50 +0530

    adding EStep class

--- estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
+++ estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
@@ -141,4 +141,27 @@ class EStepView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class EStep(var i : Int) {
+        private var root : EStepNode = EStepNode(0)
+        private var curr : EStepNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
