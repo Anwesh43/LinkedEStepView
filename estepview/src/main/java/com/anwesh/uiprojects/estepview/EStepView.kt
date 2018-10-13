710da063c6c771992dba711962d062cd0d3fb27b
Anwesh43 <anweshthecool0@gmail.com>
2018-10-13 08:51:59 +0530

    adding EStepNode class

--- estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
+++ estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
@@ -98,4 +98,47 @@ class EStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class EStepNode(var i : Int, val state : State = State()) {
+
+        private var next : EStepNode? = null
+        private var prev : EStepNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = EStepNode(i + 1)
+                next?.prev = this
+            }
+        }
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawESNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : EStepNode {
+            var curr : EStepNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
