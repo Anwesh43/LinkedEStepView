93c45f19f2124bbacd106653aab31afa3d5f48f2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-13 08:28:55 +0530

    importing libraries and defining nodes

--- estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
+++ estepview/src/main/java/com/anwesh/uiprojects/estepview/EStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.estepview
 /**
  * Created by anweshmishra on 13/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
