6320f84758e3f084f1e933c86f3ba5828ef8168b
Anwesh43 <anweshthecool0@gmail.com>
2018-10-13 21:02:19 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedestepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedestepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedestepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.estepview.EStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        EStepView.create(this)
     }
 }
